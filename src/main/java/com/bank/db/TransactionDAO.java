90709a8aa66589d8dff7e5d75c1c8790da2793c1
MChinnappa <monicaglory05@gmail.com>
2025-08-12 16:51:52 +0530

    Updated TransactionMapper and TransactionDAO on mapResultSetToEntity

--- src/main/java/com/bank/db/TransactionDAO.java
+++ src/main/java/com/bank/db/TransactionDAO.java
@@ -1,10 +1,12 @@
 package com.bank.db;
 import com.bank.entity.TransactionEntity;
+import com.bank.mapper.TransactionMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class TransactionDAO {
     public enum TransactionType {
@@ -23,7 +25,7 @@ public class TransactionDAO {
         String sql = "SELECT * FROM transactions";
         try (ResultSet rs = (ResultSet) dm.query(sql)) {
             while (rs.next()) {
-                TransactionEntity ts = mapResultSetToEntity(rs);
+                TransactionEntity ts = TransactionMapper.mapResultSetToEntity(rs);
                 list.add(ts);
             }
         }
@@ -34,7 +36,7 @@ public class TransactionDAO {
         String sql = "SELECT * FROM transactions WHERE transaction_id = '" + accountId + "'";
         try (ResultSet rs = (ResultSet) dm.query(sql)) {
             if (rs.next()) {
-                return mapResultSetToEntity(rs);
+                return TransactionMapper.mapResultSetToEntity(rs);
             }
         }
         return null;
@@ -45,7 +47,7 @@ public class TransactionDAO {
         String sql = "SELECT * FROM transactions WHERE from_account_id = " + userId + " OR to_account_id = " + userId;
         try (ResultSet rs = (ResultSet) dm.query(sql)) {
             while (rs.next()) {
-                TransactionEntity tx = mapResultSetToEntity(rs);
+                TransactionEntity tx = TransactionMapper.mapResultSetToEntity(rs);
                 list.add(tx);
             }
         }
@@ -56,19 +58,6 @@ public class TransactionDAO {
     public void updateStatus(String transactionId, String status) throws SQLException {
         String sql = "UPDATE transactions SET status = '" + status + "' WHERE transaction_id = '" + transactionId + "'";
         dm.query(sql);
-    }
-
-    private TransactionEntity mapResultSetToEntity(ResultSet rs) throws SQLException {
-        TransactionEntity ts = new TransactionEntity();
-        ts.setTransaction_id(rs.getInt("transaction_id"));
-        ts.setFrom_account_id(rs.getInt("from_account_id"));
-        ts.setTo_account_id(rs.getInt("to_account_id"));
-        ts.setTransaction_type(rs.getString("transaction_type"));
-        ts.setAmount(rs.getDouble("amount"));
-        ts.setDescription(rs.getString("description"));
-        ts.setStatus(rs.getString("status"));
-        ts.setCreatedAt(rs.getTimestamp("created_at"));
-        return ts;
     }
 
 }
