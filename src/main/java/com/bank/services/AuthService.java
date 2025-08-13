1f7cfbd4f98501d363b1ddefac7130c4ea3ca9ed
Adesh Kedlaya <adeshkedlaya03@gmail.com>
2025-08-13 18:46:33 +0530

    Resolved an error

--- src/main/java/com/bank/services/AuthService.java
+++ src/main/java/com/bank/services/AuthService.java
@@ -14,25 +14,25 @@ public class AuthService {
 private static  userDao dao;
 private static  PasswordUtil passwordUtil;
 private final UserMapper userMapper;
-private final LogService logService;
-public AuthService(userDao dao,PasswordUtil passwordUtil ,UserMapper userMapper,LogService logService){
+
+public AuthService(userDao dao,PasswordUtil passwordUtil ,UserMapper userMapper){
     this.dao=dao;
     this.passwordUtil=passwordUtil;
     this.userMapper=userMapper;
-    this.logService=logService;
+
 }
     public UserDTO validateUserCredentials(String user,String password) throws Exception,InvalidCredentialsException, UserNotfoundException {
        UserEntity userEntity=dao.getUserByUsername(user);
         String pass=userEntity.getPasswordHash();
            if(userEntity==null){
-               logService.logintoDB(-1L, LogDAO.Action.AUTHENTICATION,"Failed authentication as userDetails is not present in the DB","user_ip",LogDAO.Status.FAILURE);
+               LogService.logintoDB(-1L, LogDAO.Action.AUTHENTICATION,"Failed authentication as userDetails is not present in the DB","user_ip",LogDAO.Status.FAILURE);
                throw new UserNotfoundException();
            }
           if(!passwordUtil.VerifyPassword(password,pass)){
-              logService.logintoDB(-1L, LogDAO.Action.AUTHENTICATION,"Invalid Password","user_ip",LogDAO.Status.FAILURE);
+              LogService.logintoDB(-1L, LogDAO.Action.AUTHENTICATION,"Invalid Password","user_ip",LogDAO.Status.FAILURE);
             throw new InvalidCredentialsException();
         }
-        logService.logintoDB(userEntity.getId(), LogDAO.Action.AUTHENTICATION,"Successfully LoggedIn","user_ip",LogDAO.Status.SUCCESS);
+        LogService.logintoDB(userEntity.getId(), LogDAO.Action.AUTHENTICATION,"Successfully LoggedIn","user_ip",LogDAO.Status.SUCCESS);
         return userMapper.UserEnityToDto(userEntity);
    }
 
